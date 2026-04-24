6dee4bb2351a9621ea67c74b1320c581c1c02130
Priyanshu Varshney <priyanshuvarshney027@gmail.com>
2026-04-24 11:27:08 +0530

    add cart routes

--- ecommerce-app/index.js
+++ ecommerce-app/index.js
@@ -1,16 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { mongo } from "mongoose";
 
 const app = express();
 app.use(express.json());
 
 mongoose.connect("mongodb://localhost:27017/ecommerce")
-.then(() => console.log("MongoDB Connected Successfully"))
-.catch((err) => console.log(err));
+    .then(() => console.log("MongoDB Connected Successfully"))
+    .catch((err) => console.log(err));
 
 const userSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-
+    name: { type: String, required: true },
 });
 
 const productSchema = new mongoose.Schema({
@@ -21,9 +20,46 @@ const productSchema = new mongoose.Schema({
 
 const cartSchema = new mongoose.Schema({
     userId: mongoose.Schema.Types.ObjectId,
-    item: {
-        product_id: mongoose.Schema.Types.ObjectId,
-        
+    items: [
+        {
+            productId: mongoose.Schema.Types.ObjectId,
+            quantity: Number,
+        },
+    ],
+});
 
+const orderSchema = new mongoose.Schema({
+    userId: mongoose.Schema.Types.ObjectId,
+    items: Array,
+    totalAmount: Number,
+    status: String,
+});
+
+const User = mongoose.model("User", userSchema);
+const Product = mongoose.model("Product", productSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+const Order = mongoose.model("Order", orderSchema);
+
+app.post("/user", async(req, resp) => {
+    const user = await User.create(req.body);
+    resp.json(user);
+});
+
+app.post("/product", async(req, resp) => {
+    const product = await Product.create(req.body);
+    resp.json(product);
+});
+
+app.post("/addtocart", async(req, resp) => {
+    const {userId, productId, quantity} = req.body;
+    let cart = await Cart.findOne({userId});
+    if(!cart){
+        cart = await Cart.create({
+            userId,
+            items: ({productId, quantity}),
+        });
+    } else{
+        cart.items.push({productId, quantity});
+        await cart.save();
     }
-})
+});
